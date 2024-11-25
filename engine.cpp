84fe033395b364c4fdcf1c0513c7abb453772637
Jeremiah Morgan <JeremiahMorgan.bham@outlook.com>
2024-11-25 14:49:03 +0000

    pay_lines

--- engine.cpp
+++ engine.cpp
@@ -10,7 +10,7 @@ struct Square
 {
     int x;
     int y;
-    int symbol;
+    std::vector<int> valid_symbol;
 };
 
 /*
@@ -18,7 +18,7 @@ class Reel
 {
     std::deque<int> values;
 
-    
+
     void add_value(int value)
     {
         values.push_back(value);
@@ -41,23 +41,14 @@ class Reel
 
 }*/
 
-
 struct Reel
 {
     std::vector<int> values;
     int start = 0;
 
-    void spin(int amount) //positive for forward //negative for backward
-    {
-        start = amount % values.size();
-    }
+    virtual void spin(int amount) = 0; // positive for forward //negative for backward
 
-    std::vector<int> get_current()
-    {
-        std::vector<int> ret(values.begin() + start, values.end());
-        ret.append_range(std::vector<int>(values.begin(),values.begin() + start));
-        return ret;
-    }
+    virtual std::vector<int> get_current() = 0;
 
     Reel(std::vector<int> values)
     {
@@ -65,62 +56,76 @@ struct Reel
     }
 };
 
+struct FairStandardReel : Reel
+{
+    void spin(int amount) // positive for forward //negative for backward
+    {
+        start = amount % values.size();
+    }
 
+    std::vector<int> get_current()
+    {
+        std::vector<int> ret(values.begin() + start, values.end());
+        ret.append_range(std::vector<int>(values.begin(), values.begin() + start));
+        return ret;
+    }
 
+    FairStandardReel(std::vector<int> values) : Reel(values) {};
+};
 
-//template<typename BonusFeature, typename RNG>
+// template<typename BonusFeature, typename RNG>
 class SlotMachine
 {
     using column_t = std::vector<int>;
     std::vector<column_t> grid;
 
-    std::vector<Reel> reels;
+    std::vector<Reel *> reels;
 
-    using payline_t = std::vector<Square*>;
+    using payline_t = std::vector<Square>;
     std::vector<std::pair<bool, payline_t>> pay_lines;
 
-
-    std::unordered_map<payline_t*, int> bets;
+    std::unordered_map<payline_t *, int> bets;
 
     int active_pay_lines;
 
     int max_bet;
     int bet_per_line;
     int total_bet_per_spin;
-    //std::vector<BonusFeature> bonus_features;
+    // std::vector<BonusFeature> bonus_features;
     int coin_size;
-    int fixed_jackpot;
+    std::unordered_map<int, int> fixed_jackpots;
     int free_spins;
     float multiplier;
     float payout_percentage;
-    std::unordered_map<payline_t*, int> pay_table;
-    int progressive_jackpot;
-    //RNG random_number_generator;
+    std::unordered_map<payline_t *, int> pay_table;
+    std::unordered_map<int, int> progressive_jackpots;
+    // RNG random_number_generator;
     std::unordered_set<int> scatter_symbols;
     std::unordered_set<int> wild_symbols;
     int current_session_coins;
 
-    int spin_number;
+    int spin_number = 0;
 
 public:
-    /*SlotMachine(std::vector<Reel> reels)
+    SlotMachine() = default;
+    SlotMachine(std::vector<Reel *> reels)
     {
-        for(Reel reel : reels)
+        for (Reel *reel : reels)
         {
             this->reels.push_back(reel);
-            grid.push_back(reel.get_current());
+            grid.push_back(reel->get_current());
         }
-    };*/
+    };
 
-    void add_reel(Reel reel)
+    void add_reel(Reel *reel)
     {
         this->reels.push_back(reel);
-        this->grid.push_back(reel.get_current());
+        this->grid.push_back(reel->get_current());
     }
 
-    void add_pay_line(std::vector<Square*> pay_line)
+    void add_pay_line(std::vector<Square> pay_line, bool is_active = true)
     {
-        this->pay_lines.push_back({false, pay_line});
+        this->pay_lines.push_back({is_active, pay_line});
     }
 
     void set_pay_line_activation(int pay_line_index, bool activation)
@@ -130,7 +135,7 @@ public:
 
     void swith_pay_line_activation(int pay_line_index)
     {
-        if(!this->pay_lines[pay_line_index].first)
+        if (!this->pay_lines[pay_line_index].first)
         {
             this->pay_lines[pay_line_index].first = false;
         }
@@ -145,17 +150,17 @@ public:
         this->max_bet = max_bet;
     }
 
-    void bet_on_pay_line(payline_t* pay_line, int coins)
+    void bet_on_pay_line(payline_t *pay_line, int coins)
     {
         this->bets[pay_line] = coins;
     }
 
     void play_bonus_feature(int bonus_feature_index)
     {
-        //this->bonus_features[bonus_feature_index].play();
+        // this->bonus_features[bonus_feature_index].play();
     }
 
-    void set_pay(payline_t* pay_line, int pay)
+    void set_pay(payline_t *pay_line, int pay)
     {
         this->pay_table[pay_line] = pay;
     }
@@ -168,50 +173,140 @@ public:
         this->wild_symbols.insert(symbol);
     }
 
+    void add_fixed_jackpot(int jackpot, int coins)
+    {
+        this->fixed_jackpots.insert({jackpot, coins});
+    }
+
+    void adjust_fixed_jackpot(int jackpot, int coins)
+    {
+        if (this->fixed_jackpots.contains(jackpot))
+        {
+            this->fixed_jackpots[jackpot] += coins;
+        }
+    }
+
+    void multiply_fixed_jackpot(int jackpot, float modifier)
+    {
+        if (this->fixed_jackpots.contains(jackpot))
+        {
+            this->fixed_jackpots[jackpot] *= modifier;
+        }
+    }
+
+    void add_progressive_jackpot(int jackpot, int coins)
+    {
+        this->progressive_jackpots.insert({jackpot, coins});
+    }
+
+    void adjust_progressive_jackpot(int jackpot, int coins)
+    {
+        if (this->progressive_jackpots.contains(jackpot))
+        {
+            this->progressive_jackpots[jackpot] += coins;
+        }
+    }
+
+    void multiply_progressive_jackpot(int jackpot, float modifier)
+    {
+        if (this->progressive_jackpots.contains(jackpot))
+        {
+            this->progressive_jackpots[jackpot] *= modifier;
+        }
+    }
+
     void set_frame(int real_index)
     {
-        this->grid[real_index] = reels[real_index].get_current();
+        this->grid[real_index] = reels[real_index]->get_current();
     }
 
-    void spin()
+    bool payline_satisfied(payline_t payline)
+    {
+        for (Square s : payline)
+        {
+            bool symbol_found = false;
+            for (int symbol : s.valid_symbol)
+            {
+                if (this->grid[s.y][s.x] == symbol)
+                    symbol_found = true;
+            }
+            if(!symbol_found)
+                return false;
+        }
+        return true;
+    }
+
+    std::vector<payline_t> find_wins()
+    {
+        std::vector<payline_t> pay_lines;
+        for (const auto &[is_active, pay_line] : this->pay_lines)
+        {
+            if (is_active && this->payline_satisfied(pay_line))
+            {
+                std::cout << "winner winner chicken dinner" << std::endl;
+                pay_lines.push_back(pay_line);
+            }
+        }
+        return pay_lines;
+    }
+
+    bool spin()
     {
         int i = 0;
-        for(Reel& reel : this->reels)
+        for (Reel *reel : this->reels)
         {
-            reel.spin(rand());
+            reel->spin(rand());
             this->set_frame(i++);
         }
+
         spin_number++;
+        if(find_wins().empty())
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+
     }
 
-    void print_grid()
+    void print_grid() const
     {
-        for(column_t coloumn : this->grid)
+        std::cout << "spin count: " << spin_number << std::endl;
+        for (column_t coloumn : this->grid)
         {
-            for(int val : coloumn)
+            for (int val : coloumn)
             {
                 std::cout << "|" << val;
             }
 
             std::cout << "|" << std::endl;
         }
+        std::cout << std::endl
+                  << std::endl;
     }
-
 };
-
 
 int main()
 {
     SlotMachine sm;
-    
-    
-    sm.add_reel(std::vector<int>({1,2,3,4,5}));
-    sm.add_reel(std::vector<int>({1,2,3,4,5}));
-    sm.add_reel(std::vector<int>({1,2,3,4,5}));
-    sm.add_reel(std::vector<int>({1,2,3,4,5}));
-    sm.add_reel(std::vector<int>({1,2,3,4,5}));
-    sm.spin();
+    FairStandardReel reel_1(std::vector<int>({1, 2, 3, 4, 5}));
+    FairStandardReel reel_2(std::vector<int>({1, 2, 3, 4, 5}));
+    FairStandardReel reel_3(std::vector<int>({1, 2, 3, 4, 5}));
+    FairStandardReel reel_4(std::vector<int>({1, 2, 3, 4, 5}));
+    FairStandardReel reel_5(std::vector<int>({1, 2, 3, 4, 5}));
 
+    sm.add_reel(&reel_1);
+    sm.add_reel(&reel_2);
+    sm.add_reel(&reel_3);
+    sm.add_reel(&reel_4);
+    sm.add_reel(&reel_5);
+    sm.add_pay_line({{2, 0, {4, 5}}, {2, 1, {4, 5}}, {2, 2, {4, 5}}, {2, 3, {4, 5}}, {2, 4, {4, 5}}});
+    
+    while(!sm.spin())
+        sm.print_grid();
 
     sm.print_grid();
+
 }
